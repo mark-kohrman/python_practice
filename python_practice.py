0c84260d6d48964a8597bfe20debf9bae50bbff9
Mark-Kohrman <mark.kohrman@gmail.com>
2020-09-13 21:29:59 -0500

    adding two loops for practing python

--- python_practice.py
+++ python_practice.py
@@ -1,15 +1,15 @@
 
 # 1. Write a while loop to print the numbers 1 through 10.
-i = 1
-while i <= 10:
-  print i
-  i = i + 1
+# i = 1
+# while i <= 10:
+#   print i
+#   i = i + 1
 
 # # 2. Write a while loop that prints the word "hello" 5 times.
-i = 1
-while i <= 5:
-  print "hello"
-  i += 1
+# i = 1
+# while i <= 5:
+#   print "hello"
+#   i += 1
 
 # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
 
@@ -19,12 +19,19 @@ while i <= 5:
   
 
 # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
-i = 0
-while i <= 100:
-  print i
-  i += 1
+# i = 0
+# while i <= 100:
+#   print (i)
+#   i += 1
 
 # 5. Write a while loop that prints the number 9000 ten times.
+number = 9000
+i = 0
+while i < 10:
+    print (number)
+    i += 1
+
+
 
 # 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
 
