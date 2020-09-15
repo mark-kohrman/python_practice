759716e2893b20544d0374f50603b7939aa61895
Mark-Kohrman <mark.kohrman@gmail.com>
2020-09-14 22:10:56 -0500

    prework checkpoint1 python practice

---
+++ prework.py
@@ -0,0 +1,29 @@
+# Write code to produce the following output. Note that there are many valid solutions - for this exercise, you are required to use addition, subtraction, multiplication, and division at least once.
+# 4
+# 30
+# 12
+# 983
+
+# print(5 - 1)
+# print(24+6)
+# print(6 * 2)
+# print(2949 / 3)
+
+# Start with this line of code:
+# p 100 * 5 - 2 / 2
+# If you run the code, it will output 499. Just by adding parentheses, change the output to 150. DO NOT change any numbers or operators (*, -, / etc.)
+
+# print(100 * (5 - 2) / 2)
+
+# DEBUG: The code below has errors. Run the code to identify the line which causes an error, then fix each error one line at a time. For this exercise, do not make changes to any lines that run without error.
+
+print(1 + 1)
+print(2 * 3)
+print(9 / 3.0)
+print(8 + .2 - 3)
+print(9+4-9)
+print(9+2-2)
+print(7+3-4)
+print(8-3*2)
+print(7 + 10)
+print(4 / 4 + 4)
