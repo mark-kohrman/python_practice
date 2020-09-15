ebbf829f59584e28de148a014522ec2ef4c06d75
Mark-Kohrman <mark.kohrman@gmail.com>
2020-09-15 10:59:40 -0500

    adding variable practice for python

--- prework.py
+++ prework.py
@@ -17,13 +17,58 @@
 
 # DEBUG: The code below has errors. Run the code to identify the line which causes an error, then fix each error one line at a time. For this exercise, do not make changes to any lines that run without error.
 
-print(1 + 1)
-print(2 * 3)
-print(9 / 3.0)
-print(8 + .2 - 3)
-print(9+4-9)
-print(9+2-2)
-print(7+3-4)
-print(8-3*2)
-print(7 + 10)
-print(4 / 4 + 4)
+# print(1 + 1)
+# print(2 * 3)
+# print(9 / 3.0)
+# print(8 + .2 - 3)
+# print(9+4-9)
+# print(9+2-2)
+# print(7+3-4)
+# print(8-3*2)
+# print(7 + 10)
+# print(4 / 4 + 4)
+
+#VARIABLES
+
+# In Python, you can use any combination of upper and lower case letters to make a variable name. Most special characters aren’t allowed, but there are a few exceptions. If you run the code below, it will crash due to invalid variable names. Find the invalid variable names and delete the special characters (be sure to leave the special characters in the valid variable names alone).
+# bob7 = 50
+# bob = 50
+# bobat = 50
+# atbob = 50
+# bob_ = 50
+# _bob = 50
+# bobdollar = 50
+# dollarbob = 50
+# print(dollarbob)
+
+# In this code, the variable z is printed out on lines 4 and 6. Predict what will print out to the terminal, then run the code to see if you were right. Explain in your own words exactly why those values were printed to the terminal.
+
+# x = 40
+# y = 3
+# z = x + y
+# print(z)
+# x = 60
+# print(z)
+
+# A family wants to order 2 hamburgers without bacon, 3 cheeseburgers with bacon and 5 golden fries. Use Python code to model this situation with variables and compute the total cost of their order (not including tax).
+# hamburger = 8.95
+# cheeseburger = 9.95
+# add_bacon = 2
+# golden_fries = 2.95
+
+# total = (hamburger * 2) + ((cheeseburger + add_bacon) * 3) + (golden_fries * 5)
+# print(total)
+
+# DEBUG: Fix the code found below.
+
+# 3 = x
+# 9 = y
+# p x + y
+#ANSWER
+x = 3
+y = 9
+
+# print(x + y)
+
+#CONDITIONALS
+
