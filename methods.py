3b4e0fe8c17ef5a6c80e810aab54bf632a7daf50
Mark-Kohrman <mark.kohrman@gmail.com>
2020-09-16 17:40:44 -0500

    finishing methods python

--- methods.py
+++ methods.py
@@ -36,12 +36,12 @@
 
 # WRITE: Create a program that says "I'm thinking of a number between 1 and 100", and asks the user to guess which number the computer is thinking of. The correct answer is 33. So, if the user guesses 33, the computer should say, “You win!”. If the number is less than 33, it should say, “Too low.” If the number is greater than 33, it should say, “Too high.”
 
-print("I'm thinking of a number between 1 and 100, please guess which number:")
-number = int(input())
+# print("I'm thinking of a number between 1 and 100, please guess which number:")
+# number = int(input())
 
-if number == 33:
-  print("You win!!!")
-elif number < 33:
-  print("Too low.")
-else:
-  print("Too high.")
+# if number == 33:
+#   print("You win!!!")
+# elif number < 33:
+#   print("Too low.")
+# else:
+#   print("Too high.")
