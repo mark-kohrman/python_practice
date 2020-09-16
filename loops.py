68ef0a054ba573b70b38c6425d5a7c57926928f9
Mark-Kohrman <mark.kohrman@gmail.com>
2020-09-16 17:56:00 -0500

    adding initial loop exercise

---
+++ loops.py
@@ -0,0 +1,12 @@
+# Write a loop to generate the following output:
+# I know you are
+# But what am I?
+# I know you are
+# But what am I?
+# I know you are
+# But what am I?
+# I know you are
+# But what am I?
+for x in range(0, 4):
+  print("I know you are")
+  print("But what am I?")
