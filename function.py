3bde0971690c095bd668f90e833fc82072b8f90d
Mark-Kohrman <mark.kohrman@gmail.com>
2020-09-17 17:43:57 -0500

    adding python practice for functions (first two)

---
+++ function.py
@@ -0,0 +1,20 @@
+# Write code that converts converts and prints out four different temperatures in Fahrenheit to Celsius. (Recall the formula for this conversion is (x - 32) * 0.5556, where x is the degrees in Fahrenheit).
+
+# def fahrenheit_to_celsius(temperature):
+#   temperature = (temperature - 32) * 0.5556
+#   print(temperature)
+
+
+# fahrenheit_to_celsius(29)
+
+# When you run the code below, it should print lyrics. However, right now if you run the code, it doesn't print anything. Add the appropriate line of code to make the code run properly.
+
+
+def print_lyrics():
+   print("Now this is a story all about how")
+   print("My life got flipped turned upside down")
+   print("And I'd like to take a minute, just sit right there")
+   print("I'll tell you how I became the prince of a town called Bel-Air")
+
+print_lyrics()
+
