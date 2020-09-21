a6ea4976df0d456a3ce5601afc8b7c3de71b2f39
Mark-Kohrman <mark.kohrman@gmail.com>
2020-09-20 21:58:14 -0500

    hackerrank exercise adding strings to single line

--- hackerrank.py
+++ hackerrank.py
@@ -14,17 +14,29 @@
 
 # Note that the code stub provided reads from STDIN and passes arguments to the is_leap function. It is only necessary to complete the is_leap function.
 
-def is_leap(year):
-    leap = False
-    if(year % 4 == 0):
-      leap = True
+# def is_leap(year):
+#     leap = False
+#     if(year % 4 == 0):
+#       leap = True
     
-    if(year % 4 == 0 and year % 100 == 0):
-      leap = False
+#     if(year % 4 == 0 and year % 100 == 0):
+#       leap = False
     
-    if(year % 4 == 0 and year % 100 == 0 and year % 400 == 0):
-      leap = True
+#     if(year % 4 == 0 and year % 100 == 0 and year % 400 == 0):
+#       leap = True
     
-    return leap
+#     return leap
 
-print(is_leap(2400))
+# print(is_leap(2400))
+
+###############
+string = ""
+n = 3
+i = 1
+while i <= n:
+    string = string + str(i) 
+    i = int(i)
+    i+=1
+
+print(string)
+  
