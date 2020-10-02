e6db88b4b503f97f144e30b3daa2084bfd11381e
Mark-Kohrman <mark.kohrman@gmail.com>
2020-10-01 20:06:32 -0500

    conv list of dicts, to dict of dicts - exercise 2

--- convert.py
+++ convert.py
@@ -14,6 +14,17 @@ print(dictionary)
 
 #  2. Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
 #     For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
+items = [{"id": 1, "color": "blue", "price": 32},
+         {"id": 2, "color": "red", "price": 12}]
+item_hash = {}
+i = 0
+while i < len(items):
+    key = items[i]["id"]
+    value = items[i]
+    item_hash[key] = value
+    i += 1
+
+print(item_hash)
 
 #  3. Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
 #     For example, "bookkeeper" becomes {"b" => 1, "o" => 2, "k" => 2, "e" => 3, "p" => 1, "r" => 1}.
@@ -38,6 +49,5 @@ print(dictionary)
 
 # 10. Given a hash, create a new hash that has the keys and values switched.
 #     For example, {"a" => 1, "b" => 2, "c" => 3} becomes {1 => "a", 2 => "b", 3 => "c"}.
-
 
 # SOLUTIONS: https://gist.github.com/peterxjang/216a7a6e8411ee5c05118e78022f2bc7
